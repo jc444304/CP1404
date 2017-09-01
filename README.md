ca4d51eba904ed6596fcd02f203f7cab2d2e548c
yvan-burrie <yvan.burrie@hotmail.com>
2017-09-01 15:40:57 +1000

    More updates

---
+++ README.md
@@ -0,0 +1,2 @@
+# JCU 2017 SP2 CP1404 - Task 1
+ * **Name:** Yvan Burrie
--- main.py
+++ main.py
@@ -1,35 +1,59 @@
 import csv
 
-SONGS_CSV_FILE_NAME = "song.csv"
+
+
+SONGS_FILE = "songs.csv"
 
 SONG_YEAR_MIN = 1000
 
-"""
+SONGS_INVALID_CHARS = [
+	""
+]
 
 """
 
-
+"""
 def main():
 	print("Songs to Learn - by Yvan Burrie")
 	
 	songs = []
-	with open(SONGS_CSV_FILE_NAME, 'rb') as songsFile:
-		spamreader = csv.reader(songsFile, delimiter=' ', quotechar='|')
-		for row in spamreader:
-			print(row)
+	try:
+		with open(SONGS_FILE, 'r') as songsFile:
+			songsReader = csv.reader(songsFile, delimiter="\t")
+			for songRaw in songsReader:
+				songs.append({
+					"Title": songRaw[0],
+					"Artist": songRaw[1],
+					"Year": songRaw[2],
+					"Done": songRaw[3],
+				})
+	except IOError:
+		print("Failed to open file '{}' for reading.".format(SONGS_FILE))
 	
 	while True:
 		showMainMenu()
 		menuOption = input().lower()
 		if menuOption == "l":
-			showSongsList(songs)
+			show_songs_list(songs)
 		if menuOption == "a":
 			showSongsAdd()
 		if menuOption == "c":
-		
+			showSongsDone(songs)
 		if menuOption == "q":
 			break
-	
+	songsCount = songs.count(songs)
+	songId = 0
+	try:
+		with open(SONGS_FILE, 'w') as songsFile:
+			songId += 1
+			song = songs[songId]
+			songsWriter = csv.writer(songsFile)
+			songRaw = [
+				song['Title']
+			]
+			songsWriter.writerow(songRaw)
+	except IOError:
+		print("Failed to open file '{}' for reading.".format(SONGS_FILE))
 	print("{} song(s) saved to ")
 
 def showMainMenu():
@@ -39,9 +63,10 @@ def showMainMenu():
 	print("C - Complete a song")
 	print("Q - Quit")
 
-def showSongsList(songs):
-	songCount = songs.count()
-	songsDone = getSongsDone()
+
+def show_songs_list(songs):
+	songCount = songs.count(songs)
+	songsDone = getSongsDone(songs)
 	for song in songs:
 		print("{} {} {} {}".format(song['Title'], song['Artist'], song['Year'], song['Done']))
 	print("{} song(s) learned, {} song(s) still to learn.".format(songsDone, songCount - songsDone))
@@ -54,7 +79,20 @@ def getSongsDone(songs):
 	return songsDone
 
 def showSongsAdd():
-	pass
+	print("Attempting to add song...")
+	while True:
+		songTitle = input("Title: ")
+		if not songTitle:
+			print("Title must not be empty!")
+			continue
+		songArtist = input("Artist: ")
+		if not songArtist:
+			print("Artist must be empty!")
+			continue
+		print("")
+	songs.append({
+		"Title": songTitle,
+	})
 
 def showSongsDone(songs):
 	songsCount = songs.count()
